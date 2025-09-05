86b71f7f8e42c1388901a6c9849c25cfd910d551
yuita-yoshihiko <soup998211@gmail.com>
2025-09-05 16:26:37 +0900

    design_tipとcategoryを一緒に返すrepositoryのテスト

--- adapter/database/design_tip_test.go
+++ adapter/database/design_tip_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/yuita-yoshihiko/daredemo-design-backend/adapter/database"
 	"github.com/yuita-yoshihiko/daredemo-design-backend/infrastructure/db"
 	"github.com/yuita-yoshihiko/daredemo-design-backend/models"
+	"github.com/yuita-yoshihiko/daredemo-design-backend/models/custom"
 	"github.com/yuita-yoshihiko/daredemo-design-backend/testutils"
 )
 
@@ -22,76 +23,54 @@ func Test_DesignTip_Fetch(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want *models.DesignTip
+		want *custom.DesignTipWithCategories
 	}{
 		{
 			name: "idで抽出した単一のDesignTipのデータが取得できる",
 			args: args{
 				id: 1,
 			},
-			want: &models.DesignTip{
-				ID:        1,
-				Title:     "テストタイトル",
-				Guidance:  "テストガイダンス",
-				URL:       "https://test.com",
-				Media:     "book",
-				CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
-				UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+			want: &custom.DesignTipWithCategories{
+				DesignTip: models.DesignTip{
+					ID:        1,
+					Title:     "テストタイトル",
+					Guidance:  "テストガイダンス",
+					URL:       "https://test.com",
+					Media:     "book",
+					CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+					UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+				},
+				Categories: []models.Category{
+					{
+						ID:        1,
+						Name:      "テストカテゴリ1",
+						CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+					},
+					{
+						ID:        2,
+						Name:      "テストカテゴリ2",
+						CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+					},
+					{
+						ID:        3,
+						Name:      "テストカテゴリ3",
+						CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+					},
+				},
 			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := r.Fetch(t.Context(), tt.args.id)
+			got, err := r.FetchWithCategories(t.Context(), tt.args.id)
 			if err != nil {
 				t.Errorf("error = %v", err)
 			}
-			testutils.AssertResponse(t, got, tt.want, models.DesignTip{})
-		})
-	}
-}
-
-func Test_DesignTip_Create(t *testing.T) {
-	data := testutils.LoadFixture(t, "testfixtures/design_tips/create")
-	dbUtils := db.NewDBUtil(data)
-	r := database.NewDesignTipRepository(dbUtils)
-
-	tests := []struct {
-		name  string
-		input *models.DesignTip
-		want  *models.DesignTip
-	}{
-		{
-			name: "正常にDesignTipのデータが作成できる",
-			input: &models.DesignTip{
-				Title:    "テストタイトル",
-				Guidance: "テストガイダンス",
-				URL:      "https://test.com",
-				Media:    "book",
-			},
-			want: &models.DesignTip{
-				Title:    "テストタイトル",
-				Guidance: "テストガイダンス",
-				URL:      "https://test.com",
-				Media:    "book",
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			id, err := r.Create(t.Context(), tt.input)
-			if err != nil {
-				t.Errorf("error = %v", err)
-			}
-			got, err := r.Fetch(t.Context(), id)
-			if err != nil {
-				t.Errorf("error = %v", err)
-			}
-			opt1 := testutils.DefaultIgnoreFieldsOptions(models.DesignTip{})
-			opt2 := testutils.GenerateIgnoreUnexportedTypesOptions(models.DesignTip{})
-			testutils.AssertResponseWithOptions(t, got, tt.want, opt1, opt2)
+			testutils.AssertResponse(t, got, tt.want)
 		})
 	}
 }
