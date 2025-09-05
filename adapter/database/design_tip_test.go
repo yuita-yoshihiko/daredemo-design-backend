89d79f6a5506698221ca2b70d4354eec7841def6
yuita-yoshihiko <soup998211@gmail.com>
2025-09-05 17:01:31 +0900

    全てのdesign_tipをcategoryと紐付けて取得するrepositoryのテスト

--- adapter/database/design_tip_test.go
+++ adapter/database/design_tip_test.go
@@ -11,8 +11,8 @@ import (
 	"github.com/yuita-yoshihiko/daredemo-design-backend/testutils"
 )
 
-func Test_DesignTip_Fetch(t *testing.T) {
-	data := testutils.LoadFixture(t, "testfixtures/design_tips/fetch")
+func Test_DesignTip_FetchWithCategories(t *testing.T) {
+	data := testutils.LoadFixture(t, "testfixtures/design_tips/fetch_with_categories")
 	dbUtils := db.NewDBUtil(data)
 	r := database.NewDesignTipRepository(dbUtils)
 
@@ -26,7 +26,7 @@ func Test_DesignTip_Fetch(t *testing.T) {
 		want *custom.DesignTipWithCategories
 	}{
 		{
-			name: "idで抽出した単一のDesignTipのデータが取得できる",
+			name: "idで抽出した単一のDesignTipのデータがCategoryを紐づけて取得できる",
 			args: args{
 				id: 1,
 			},
@@ -67,6 +67,102 @@ func Test_DesignTip_Fetch(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := r.FetchWithCategories(t.Context(), tt.args.id)
+			if err != nil {
+				t.Errorf("error = %v", err)
+			}
+			testutils.AssertResponse(t, got, tt.want)
+		})
+	}
+}
+
+func Test_DesignTip_FetchAllWithCategories(t *testing.T) {
+	data := testutils.LoadFixture(t, "testfixtures/design_tips/fetch_all_with_categories")
+	dbUtils := db.NewDBUtil(data)
+	r := database.NewDesignTipRepository(dbUtils)
+
+	tests := []struct {
+		name string
+		want []*custom.DesignTipWithCategories
+	}{
+		{
+			name: "全てのDesignTipのデータがCategoryを紐づけて取得できる",
+			want: []*custom.DesignTipWithCategories{
+				{
+					DesignTip: models.DesignTip{
+						ID:        1,
+						Title:     "テストタイトル",
+						Guidance:  "テストガイダンス",
+						URL:       "https://test.com",
+						Media:     "book",
+						CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+					},
+					Categories: []models.Category{
+						{
+							ID:        1,
+							Name:      "テストカテゴリ1",
+							CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+							UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						},
+						{
+							ID:        2,
+							Name:      "テストカテゴリ2",
+							CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+							UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						},
+						{
+							ID:        3,
+							Name:      "テストカテゴリ3",
+							CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+							UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						},
+					},
+				},
+				{
+					DesignTip: models.DesignTip{
+						ID:        2,
+						Title:     "テストタイトル2",
+						Guidance:  "テストガイダンス2",
+						URL:       "https://test2.com",
+						Media:     "book",
+						CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+					},
+					Categories: []models.Category{
+						{
+							ID:        1,
+							Name:      "テストカテゴリ1",
+							CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+							UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						},
+					},
+				},
+				{
+					DesignTip: models.DesignTip{
+						ID:        3,
+						Title:     "テストタイトル3",
+						Guidance:  "テストガイダンス3",
+						URL:       "https://test3.com",
+						Media:     "book",
+						CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+					},
+					Categories: []models.Category{
+						{
+							ID:        1,
+							Name:      "テストカテゴリ1",
+							CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+							UpdatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := r.FetchAllWithCategories(t.Context())
 			if err != nil {
 				t.Errorf("error = %v", err)
 			}
